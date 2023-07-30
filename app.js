9483bbbbb12f8875f302a8666b47ee6fc36a1c52
PraneethPedapati <praneethpedapati98@gmail.com>
2023-07-30 06:41:16 +0000

    added 1,2,3 scenarios

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server running on http://localhost:3000");
+    });
+  } catch (err) {
+    console.log("Error: ", err);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/todos", async (req, res) => {
+  let { status, priority } = req.query;
+
+  status = status ? status : "%%%";
+  priority = priority ? priority : "%%%";
+
+  const sql = `SELECT id, todo, priority, status, category, due_date AS dueDate FROM todo
+  WHERE status LIKE "${status}"
+  AND priority LIKE "${priority}"`;
+
+  let resp = await db.all(sql);
+  res.send(resp);
+});
