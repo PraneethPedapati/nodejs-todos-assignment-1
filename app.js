285c6d19b6b5426f8b0f2f371c8d2c1e178bddd2
PraneethPedapati <praneethpedapati98@gmail.com>
2023-07-30 06:46:54 +0000

    API-1 added

--- app.js
+++ app.js
@@ -28,14 +28,20 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 
 app.get("/todos", async (req, res) => {
-  let { status, priority } = req.query;
+  let { status, priority, category, search_q } = req.query;
 
-  status = status ? status : "%%%";
-  priority = priority ? priority : "%%%";
+  status = status ? status : "%%";
+  priority = priority ? priority : "%%";
+  category = category ? category : "%%";
+  search_q = search_q ? search_q : "%%";
 
   const sql = `SELECT id, todo, priority, status, category, due_date AS dueDate FROM todo
   WHERE status LIKE "${status}"
-  AND priority LIKE "${priority}"`;
+  AND priority LIKE "${priority}"
+  AND category LIKE "${category}"
+  AND todo LIKE "${search_q}"`;
+
+  console.log("sql", sql);
 
   let resp = await db.all(sql);
   res.send(resp);
